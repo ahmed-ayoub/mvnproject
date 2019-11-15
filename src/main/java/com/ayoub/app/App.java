f8c69e798b27161ad35f087e7d41ea2023a161f1
a.ayoub <a.ayoub@cequens.com>
2019-11-15 13:48:29 +0100

    add some wrong code

--- src/main/java/com/ayoub/app/App.java
+++ src/main/java/com/ayoub/app/App.java
@@ -8,8 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        int x=0;
-        int y=0;
+        int z=0;
         System.out.println( "Hello World....!" );
     }
 }
